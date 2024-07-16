cb50ba054b2c48a94bd6ed8b1331ef7f7e5fc44b
andreighflorin <96766497+andreighflorin@users.noreply.github.com>
2024-07-16 12:46:46 +0300

    Add files via upload

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,11 @@
+import styles from "./Header.module.css";
+
+const Header = () => {
+  return (
+    <div className={styles.header}>
+      <h2>Countries App</h2>
+    </div>
+  );
+};
+
+export default Header;
